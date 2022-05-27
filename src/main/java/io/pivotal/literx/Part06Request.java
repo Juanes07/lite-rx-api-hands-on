0b83db2bdecaba37ce4d0cb9b7d883571ca93cde
Juanes07 <juanesdm3@gmail.com>
2022-05-26 21:07:02 -0500

    Part06
    Se resuelve ejercicio 6

--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -19,28 +19,36 @@ public class Part06Request {
 
 	// TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
 	StepVerifier requestAllExpectFour(Flux<User> flux) {
-		return null;
+		StepVerifier stepVerifier = StepVerifier.create((flux)).expectNextCount(4).expectComplete();
+		return stepVerifier;
 	}
 
 //========================================================================================
 
 	// TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE then stops verifying by cancelling the source
 	StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
-		return null;
+		StepVerifier stepVerifier = StepVerifier.create((flux)).thenRequest(1).expectNext(User.SKYLER).thenRequest(1).expectNext(User.JESSE).thenCancel().log();
+		return stepVerifier;
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
 	Flux<User> fluxWithLog() {
-		return null;
+		Flux<User> users = repository.findAll().log();
+		return users;
 	}
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
+	// TODO Return a Flux with all users stored in the repository that prints "Starring:"
+	//  on subscribe, "firstname lastname" for all values and "The end!" on complete
 	Flux<User> fluxWithDoOnPrintln() {
-		return null;
+		Flux<User> users = repository.findAll()
+				.doOnSubscribe(user-> System.out.println("Starring:"))
+				.doOnNext(firstAndLastName -> System.out.println(firstAndLastName.getFirstname()+ " " + firstAndLastName.getLastname()))
+				.doOnComplete(() -> System.out.println("The end!"));
+		return users;
 	}
 
 }
