06e5e275e2fc6f3c91cbb0a22c22aa546aa53885
Juanes07 <juanesdm3@gmail.com>
2022-05-26 21:51:15 -0500

    Part07
    Ejercicio Resuelto Part07

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -31,35 +31,48 @@ public class Part07Errors {
 
 //========================================================================================
 
-	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
-	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
-	}
+    // TODO Return a Mono<User> containing User.SAUL when an error
+    //  occurs in the input Mono, else do not change the input Mono.
+    Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
+        Mono<User> userError = mono.onErrorReturn(User.SAUL);
+        return userError;
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
-	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
-	}
+    // TODO Return a Flux<User> containing User.SAUL and User.JESSE
+    //  when an error occurs in the input Flux, else do not change the input Flux.
+    Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
+        Flux<User> usersError = flux.onErrorResume(errors -> Flux.just(User.SAUL, User.JESSE));
+        return usersError;
+    }
 
 //========================================================================================
 
-	// TODO Implement a method that capitalizes each user of the incoming flux using the
-	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    // TODO Implement a method that capitalizes each user of the incoming flux using the
+    // #capitalizeUser method and emits an error containing a GetOutOfHereException error
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        Flux<User> users = flux.map(user -> {
+            try {
+                return capitalizeUser(user);
 
-	User capitalizeUser(User user) throws GetOutOfHereException {
-		if (user.equals(User.SAUL)) {
-			throw new GetOutOfHereException();
-		}
-		return new User(user.getUsername(), user.getFirstname(), user.getLastname());
-	}
+            } catch (GetOutOfHereException exc) {
 
-	protected final class GetOutOfHereException extends Exception {
-	    private static final long serialVersionUID = 0L;
-	}
+                throw Exceptions.propagate(exc);
+            }
+        });
+        return users;
+    }
+
+    User capitalizeUser(User user) throws GetOutOfHereException {
+        if (user.equals(User.SAUL)) {
+            throw new GetOutOfHereException();
+        }
+        return new User(user.getUsername(), user.getFirstname(), user.getLastname());
+    }
+
+    protected final class GetOutOfHereException extends Exception {
+        private static final long serialVersionUID = 0L;
+    }
 
 }
