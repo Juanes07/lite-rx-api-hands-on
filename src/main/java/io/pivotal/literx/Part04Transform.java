98ecb9520f57b05058076d57442133df17903e81
Juanes07 <juanesdm3@gmail.com>
2022-05-26 20:30:00 -0500

    Part05
    Se resuelve ejercicio  5 metodos Merge

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -4,7 +4,6 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Locale;
 
 /**
  * Learn how to transform values.
--- src/main/java/io/pivotal/literx/Part05Merge.java
+++ src/main/java/io/pivotal/literx/Part05Merge.java
@@ -15,21 +15,24 @@ public class Part05Merge {
 
 	// TODO Merge flux1 and flux2 values with interleave
 	Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		Flux combine = flux1.mergeWith(flux2);
+		return combine;
 	}
 
 //========================================================================================
 
 	// TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
 	Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		Flux combine = flux1.concatWith(flux2);
+		return combine;
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux containing the value of mono1 then the value of mono2
 	Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		Flux combine = Flux.concat(mono1,mono2);
+		return combine;
 	}
 
 }
