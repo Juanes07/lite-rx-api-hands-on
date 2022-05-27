0d40315cdb09e8e7278bd2424fa1a68d5ec3d455
Juanes07 <juanesdm3@gmail.com>
2022-05-27 07:11:20 -0500

    Part11
    Se resuelve ejercicio 11 pasando test

--- src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
+++ src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
@@ -25,16 +25,24 @@ public class Part11BlockingToReactive {
 
 //========================================================================================
 
-	// TODO Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
+	// TODO Create a Flux for reading all users from the blocking repository deferred
+	//  until the flux is subscribed, and run it with an elastic scheduler
 	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
-		return null;
+		Flux<User> usersReading =  Flux.defer(
+				() -> Flux.fromIterable(repository.findAll()))
+				.subscribeOn(Schedulers.elastic());
+		return usersReading;
 	}
 
 //========================================================================================
 
-	// TODO Insert users contained in the Flux parameter in the blocking repository using an elastic scheduler and return a Mono<Void> that signal the end of the operation
+	// TODO Insert users contained in the Flux parameter in the blocking repository
+	//  using an elastic scheduler and return a Mono<Void> that signal the end of the operation
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
-		return null;
+		Mono<Void> userContained = flux.publishOn(Schedulers.elastic())
+				.doOnNext(u -> repository.save(u))
+				.then();
+		return userContained;
 	}
 
 }
