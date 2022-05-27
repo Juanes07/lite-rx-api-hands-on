23c5c2196015c995087d7e4b95ad551a0b67b6ac
Juanes07 <juanesdm3@gmail.com>
2022-05-26 20:08:25 -0500

    Part04
    Se completa ejercicio Part04 utilizando Flux y Mono

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -4,6 +4,8 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Locale;
+
 /**
  * Learn how to transform values.
  *
@@ -15,21 +17,30 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		Mono letterCapitalize = mono.map(user ->
+				new User(user.getUsername().toUpperCase(),
+						user.getFirstname().toUpperCase(),
+						user.getLastname().toUpperCase()));
+		return letterCapitalize;
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		Flux letterCapitalize = flux.map(user -> new User(
+				user.getUsername().toUpperCase(),
+				user.getFirstname().toUpperCase(),
+				user.getLastname().toUpperCase()));
+		return letterCapitalize;
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		Flux<User> userData = flux.flatMap(user -> asyncCapitalizeUser(user));
+		return  userData;
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
